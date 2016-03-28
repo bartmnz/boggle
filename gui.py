a3b7129559c260bb4aafbb4031ad49ccf6a560b2
sbartholomew <sbartholomew@TCLinuxLab>
2016-03-28 08:50:16 -0400

    removed calls to sleep -- replaced with exit flag

--- gui.py
+++ gui.py
@@ -52,7 +52,7 @@ class GuiPart:
         frame3 = Frame(win)
         #frame3.pack()
         scroll = Scrollbar(frame3, orient=VERTICAL)
-        self.select = Listbox(frame3, height= 9)
+        self.select = Listbox(frame3, height= 18)
         
         #elect.configure(anchor= CENTER)
         self.select.pack()
@@ -65,7 +65,7 @@ class GuiPart:
         
         frame5 = Frame(win)
         #frame5.pack()
-        self.userWords = Listbox(frame5, height= 9)
+        self.userWords = Listbox(frame5, height= 18)
         self.userWords.pack()
         frame1.grid(row = 1, column = 1)
         frame2.grid(row = 2, column = 1)
@@ -99,6 +99,7 @@ class threadedClient:
         self.line = queue.Queue()
         self.gui = GuiPart(master, self.line, self.endApplication, self.board, self.solutions)
         self.running = 1
+        self.exit_flag = threading.Event()
         self.thread1 = threading.Thread(target = self.aiThread1)
         self.thread2 = threading.Thread(target = self.clockThread2)
         self.thread1.start()
@@ -113,21 +114,24 @@ class threadedClient:
             sys.exit(1)
         self.master.after(100, self.updateList)
         
-    def aiThread1(self):
-        while self.running:
-            time.sleep(2)
+    def aiThread1(self):            
+        DELAY = 10
+        while not self.exit_flag.wait(timeout=DELAY):
+            #time.sleep(10)
             import random
             rValue = random.sample(self.solutions, 1)
             self.line.put(rValue)
     
     def clockThread2(self):
-        while self.running:
+        DELAY = 1
+        while not self.exit_flag.wait(timeout=DELAY):
             self.gui.countDown()
-            time.sleep(1)
+            #time.sleep(1)
         
     
     def endApplication(self):
         self.running = 0
+        self.exit_flag.set()
         self.thread1.join(timeout= None)
         self.thread2.join(timeout= None)
         
@@ -140,7 +144,7 @@ def main():
         ai.endApplication()
         win.destroy()
     win.protocol("WM_DELETE_WINDOW", on_closing)
-    win.wm_title("Boggle")
+    win.wm_title("Boggle-ish")
     mainloop()
     ai.endApplication()
 
